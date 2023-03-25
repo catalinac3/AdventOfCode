f3a45636596225084f2798e49c02238b67a99b93
catalina <cdianacatalina@gmail.com>
2023-03-25 12:08:45 +0100

    day3 part b

--- adventofcode/day3.py
+++ adventofcode/day3.py
@@ -1,13 +1,22 @@
 import utils
-from aocd import submit
+# from aocd import submit
 
 dataList = utils.readData(2022, 3,'\n')
+
+
+def repeatedItem(items:list):
+    result = set(items[0])
+    otherItems = items[1:]
+    for item in otherItems:
+        result = result.intersection(set(item))
+    (item, ) = result
+    return item
 
 def takeRepeatedType(string):
     half = len(string)//2
     firstHalf = string[:half]
     secondHalf = string[half:]
-    repeatedElement = list(set(firstHalf).intersection(set(secondHalf)))[0]
+    repeatedElement = repeatedItem([firstHalf,secondHalf])
     return repeatedElement
 
 def getRepeatedElements(dataList):
@@ -27,12 +36,53 @@ def createDictLetter():
         value +=1
     return letters_dict
 
-letter_dict = createDictLetter()
-
-def calculateResult():
-    repeatedElements = getRepeatedElements(dataList)
+def calculateSumWithLetterWeight(letterList):
     letter_dict = createDictLetter()
     sum = 0
-    for letter in repeatedElements:
+    for letter in letterList:
         sum += letter_dict[letter]
     return sum
+    
+def calculateResultPartA():
+    repeatedElements = getRepeatedElements(dataList)
+    return calculateSumWithLetterWeight(repeatedElements)
+
+print(calculateResultPartA())
+
+# print(dataList)
+
+# plan:
+# loop through the data every 3 items
+# find common item for those 3 items - function
+# find the priority add it to the sum  
+
+def getAllRepeatedItems(dataList):
+    # for each 3 items on the list get all repeated items
+    i = 0 
+    j = 3
+    lastItem = len(dataList)
+    repeatedLetters = []
+    while j != lastItem:
+        repeatedLetters.append(repeatedItem(dataList[i: j]))
+        i = j 
+        j = j + 3
+    # adding the last 3 items
+    repeatedLetters.append(repeatedItem(dataList[i:]))
+  
+    return repeatedLetters
+
+def calculateResultPartB():
+    repeatedElements = getAllRepeatedItems(dataList)
+    return calculateSumWithLetterWeight(list(repeatedElements))
+
+# test 
+listExample = ['vJrwpWtwJgWrhcsFMMfFFhFp','jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL','PmmdzqPrVvPwwTWBwg']
+listExample1 = ['wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn','ttgJtRGJQctTZtZT','CrZsJsPPZsGzwwsLwLmpwMDw']
+print(repeatedItem(listExample)) # ---> {'r'}
+print(repeatedItem(listExample1)) # ---> {Z}
+
+testList = listExample + listExample1
+print(getAllRepeatedItems(testList)) # ---> {'r','Z'}
+print(getAllRepeatedItems(dataList)) # ---> {'r','Z'}
+
+print(calculateResultPartB())
