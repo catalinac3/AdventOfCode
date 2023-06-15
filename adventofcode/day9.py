9c942f5bddc8cdc6a09665ba134d7a5207f8cf6e
catalina <cdianacatalina@gmail.com>
2023-06-15 22:02:35 +0200

    day9-partb

--- adventofcode/day9.py
+++ adventofcode/day9.py
@@ -3,6 +3,7 @@ import utils
 dataList = utils.readData(2022, 9, "\n")
 
 dataExample = ['R 4','U 4','L 3', 'D 1', 'R 4', 'D 1', 'L 5','R 2']
+secondDataExample = ['R 5','U 8','L 8', 'D 3', 'R 17', 'D 10', 'L 25', 'U 20']
 
 def headMoves(direction, headCurrentPosition):
     '''moves always just 1 step'''
@@ -35,7 +36,6 @@ def tailMoves(direction, headCurrentPosition, tailsCurrentPosition):
         yT = yH
     elif direction == 'R':
         #tail ends up left to head
-
         xT = xH - 1
         yT = yH
     elif direction == 'U':
@@ -63,8 +63,49 @@ def calculatePositionsVisited(dataList):
             positionsVisitedByTail.add(tailsCurrentPosition)
     return len(positionsVisitedByTail) 
 
+def tailMovesAlternative(headCurrentPosition, tailsCurrentPosition):
+    '''catches up with head if necessary
+    alternative solution'''
+
+    xH, yH = headCurrentPosition
+    xT, yT = tailsCurrentPosition
+    
+    # Move on the x-axis:
+    if xH > xT:
+        xT += 1
+
+    if xH < xT:
+        xT -= 1
+
+    if yH > yT:
+        yT +=1
+
+    if yH < yT:
+        yT -=1
+        
+    return (xT, yT)
+
+def calculatePositionsVisitedTail9(dataList):
+    positionsVisitedByTail9 = {(0,0)}
+    headCurrentPosition = (0,0)
+    tailCurrentPositions = [(0,0) for _ in range(9)]
+    # print(tailCurrentPositions)
+    for data in dataList:
+        direction, steps = data.split(' ')
+        # One move
+        for _ in range(int(steps)):
+            headCurrentPosition = headMoves(direction, headCurrentPosition)
+            changingHead = headCurrentPosition
+            for index, tailCurrentPosition in enumerate(tailCurrentPositions):
+                # print(index,"tail:", tailCurrentPosition,"changing head:", changingHead)
+                if not isTailAdjacent(changingHead, tailCurrentPosition):
+                    tailCurrentPositions[index] = tailMovesAlternative(changingHead, tailCurrentPosition)
+                changingHead = tailCurrentPositions[index] 
+                positionsVisitedByTail9.add(tailCurrentPositions[8])
+    
+    return len(positionsVisitedByTail9) 
 
 
+# print(calculatePositionsVisited(dataList))
 
-
-print(calculatePositionsVisited(dataList))
+# print("tail9", calculatePositionsVisitedTail9(dataList))
